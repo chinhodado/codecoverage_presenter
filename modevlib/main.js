6523a2b89b53a49198e20c28c968507ac142b199
Trung Do <chin.bimbo@gmail.com>
2016-03-17 19:14:38 -0400

    Don't import cccChart.js since it causes error

--- modevlib/main.js
+++ modevlib/main.js
@@ -7,7 +7,6 @@ importScript("rest/BugzillaClient.js");
 importScript("rest/ElasticSearch.js");
 importScript("qb/ESQuery.js");
 importScript("Bugzilla.js");
-importScript("charts/cccChart.js");
 importScript("charts/aColor.js");
 importScript([
   "gui/GUI.js",
