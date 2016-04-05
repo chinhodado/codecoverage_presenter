941ea758ead2998a7210a0bf015f56ebc3b351fd
Gregory Mierzwinski <gmierz1@live.ca>
2016-04-05 15:33:17 -0400

    Fixed to get results and added common source file query.

--- ccov_api.js
+++ ccov_api.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
 *
 * This is the interface for all types of queries. It is used in JsonCcov
@@ -10,13 +11,12 @@
 
 
 function Query() {
-    "use strict";
 }
 
-Query.prototype.performQuery = function () {
-    "use strict";
-    var x = {};
-    return x;
+Query.prototype.performQuery = function (callback) {
+    
+    console.log("here");
+    return callback;
 };
 
 /**
@@ -28,11 +28,11 @@ Query.prototype.performQuery = function () {
 *
 * Use setQuries() if multiple queries are to be done. getQueriesResults()
 * must be used after using this if all the queries are to be executed
-* at once. 
+* at once.
 *
 * Use getQueryResults to execute the query given through setQuery.
 * By default, if setQuery() was not called beforehand with a query type,
-* it will return nothing. Otherwise, it will return a json formatted 
+* it will return nothing. Otherwise, it will return a json formatted
 * object that will contain the results.
 *
 * Use getQueriesResults() to execute all the queries given through setQueries().
@@ -54,12 +54,10 @@ JsonCcov.prototype.setQuery = function (queryTypeToDo) {
 };
 
 JsonCcov.prototype.setQueries = function (queriesToDo) {
-    "use strict";
     this.queries = queriesToDo;
 };
 
-JsonCcov.prototype.storeResults = function(source, callback){
-    this.result = source;
+JsonCcov.prototype.storeResults = function(callback){
     console.log("This next line is output once the query is finished. It has values in the object.")
     console.log(this.result);
     return callback();
@@ -70,31 +68,25 @@ JsonCcov.prototype._callBack = function(source){
     return source;
 };
 
-JsonCcov.prototype.getQueryResults = function () {
+JsonCcov.prototype.getQueryResults = function (callback) {
     console.log(this.queryType);
     if (this.queries && !this.queryType) {
         return {};
     }
-    
     var queryDone = this.queryType;
-    
-    return queryDone.performQuery(this.storeResults, this._callBack);
-    console.log(this.result);
-    
-    return this.result;
+    return queryDone.performQuery(callback);
 };
 
 JsonCcov.prototype.getQueriesResults = function () {
-    "use strict";
     if (!this.queries) {
         return {};
     }
     var results = [];
-    
+
     this.queries.forEach(function (element, index) {
         results.add(element.performQuery());
     });
-    
+
     return results;
 };
 
@@ -107,15 +99,14 @@ JsonCcov.prototype.getQueriesResults = function () {
 */
 
 
-    
-var search = function*(query){
-        var output = yield (Rest.post({
-            url: "https://activedata.allizom.org/query",
-            json: query
-        }));
-        yield (output);
-    };
 
+var search = function(query, callback){
+    $.ajax("https://activedata.allizom.org/query", {
+        type: "POST",
+        data: JSON.stringify(query),
+        success: callback
+    });
+};
 
 
 /**
@@ -128,35 +119,96 @@ function QueryFilesOfTest(test) {
 }
 QueryFilesOfTest.prototype = Object.create(Query.prototype);
 
-QueryFilesOfTest.prototype.performQuery = function (fn, callback) {
+QueryFilesOfTest.prototype.performQuery = function (callback) {
     var testToDo = this.testName;
-    var results; 
-    
-    importScript(['modevlib/main.js'], function () {
-        
-            Thread.run(function*(){
-                var sourceFiles = yield (search({
-                    "limit": 10000,
-                    "where": {"eq":{"test.url": testToDo}},
-                    "groupby": ["source.file"],
-                    "from": "coverage"
-                }));
-                /**
-                sourceFiles.data.forEach(function(element,index,array) {
-                   console.log(element); 
-                });**/
-                return fn(sourceFiles, callback);
-                fn(sourceFiles);
-            });
-    });
+
+    search(
+      {
+          "limit": 10000,
+          "where": testToDo,
+          "groupby": ["source.file"],
+          "from": "coverage"
+      },
+      callback
+    );
 };
 
 
+function QueryCommonFiles(testParams) {
+    Query.call(this);
+    this.testParameters = testParams;
+}
+QueryCommonFiles.prototype = Object.create(Query.prototype);
+    
+QueryCommonFiles.prototype.performQuery = function (callback) {
+        var testToDo = this.testParameters;
+        
+        var coverage = null; 
+        var sources_by_test={};
+        var commonSources = null;
+    
+        search({
+            "from":"coverage",
+            "where":testToDo,
+            "groupby":[
+                {"name":"test", "value":"test.url"},
+                {"name":"source", "value":"source.file"}
+            ],
+            "limit":10000,
+            "format":"list"
+            },
+            function(coverage) {
+                //MAP EACH TEST TO THE SET OF FILES COVERED
+                coverage.data.forEach(function(d, i){
+                    sources_by_test[d.test] = coalesce(sources_by_test[d.test], {});
+                    sources_by_test[d.test][d.source]=true;  // USE THE KEYS OF THE OBJECT AS SET
+                });
+
+                //FIND THE INTERSECTION OF COVERED FILES
+                Map.forall(sources_by_test, function(test, sourceList){
+                    if (commonSources==null) {
+                        commonSources = Map.keys(sourceList);
+                    }else{
+                        commonSources = commonSources.intersect(Map.keys(sourceList));
+                    }//endif
+                }); 
+              } 
+            );
+        
+        
+        
+        var coverage;
+        search(
+            {
+            "from":"coverage",
+            "where":testToDo,
+            "edges":[
+                {"name":"source", "value":"source.file"},
+                {"name": "test", "value": "test.url", "allowNulls": false}
+            ],
+            "limit":10000,
+            "format":"cube"
+            },
+            function(coverage) { 
+                //edges[0] DESCRIBES THE source DIMENSION, WE SELECT ALL PARTS OF THE DOMAIN
+                var all_sources = coverage.edges[0].domain.partitions.select("value");
+                //DATA IS IN {"count": [source][test]} PIVOT TABLE
+                var commonSources=[];
+                coverage.data.count.forall(function(tests, i){
+                    //VERIFY THIS source TOUCHES ALL TESTS (count>0)
+                    if (Array.AND(tests.map(function(v){return v>0;}))) {
+                        commonSources.append(all_sources[i]);
+                    }//endif
+                });
+                callback(commonSources);
+            }
+        );
+}
+
+
 function QueryCustom() {
-    "use strict";
 }
 QueryCustom.prototype = Object.create(Query.prototype);
 
 QueryCustom.prototype.performQuery = function () {
-    "use strict";
 };
